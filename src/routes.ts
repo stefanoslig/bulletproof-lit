26ca4e9550b20c22328c0005e4e6c93986e62866
stefanoslig <stefanoslignos@gmail.com>
2024-10-06 21:59:21 +0200

    feat: add libs

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+// import { Route } from '@vaadin/router';
+
+// export const routes: Route[] = [
+//   {
+//     path: '/',
+//     component: 'home-page',
+//     action: async () => {
+//       await import('./pages/home.js');
+//     },
+//   },
+//   { path: '(.*)', redirect: '/' },
+// ];

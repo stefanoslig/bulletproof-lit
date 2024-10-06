46d7b73d1feeb0ea5056ddd6a1108aace510f93d
stefanoslig <stefanoslignos@gmail.com>
2024-10-06 18:14:39 +0200

    feat: initial vite config

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  stories: ['../out-tsc/stories/**/*.stories.{js,md,mdx}'],
-  framework: {
-    name: '@web/storybook-framework-web-components',
-  },
-};
-
-export default config;
